7189a079631b73e8f51e73ba69c29813ba109c34
Jose Robles <jose.robles@opensolutions.net>
2021-03-09 13:24:14 +0100

    First upload, new public module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Alliantum Clean Barcode",
+
+    'summary': """
+        Generates a svg vector image of a barcode in order to embed it into a pdf""",
+
+
+    'author': "Alliantum",
+    'website': "https://www.alliantum.com/",
+    'category': 'Uncategorized',
+    'license': 'AGPL-3',
+    'version': '1.0.0',
+    'depends': ['base'],
+    'data': []
+}
---
+++ models/ir_actions_report.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+from odoo.addons.base.models.ir_qweb import IrQWeb
+from reportlab.graphics.barcode import createBarcodeImageInMemory
+from odoo.exceptions import UserError
+import reportlab
+import base64
+
+
+class IrActionsReport(models.Model):
+    _inherit = 'ir.actions.report'
+
+    @api.model
+    def svg_barcode(self, barcode_type, value, width=300, height=30, humanreadable=0):
+        """ Create a SVG barcode image.
+
+        Returns a base64 svg image ready to put it on a `src` attribute of an `<img>` tag
+        """
+        # All this code is based on hte original method to produce barcodes 
+        if reportlab.__version__ >= '3.5.49':
+            if barcode_type == 'UPCA' and len(value) in (11, 12, 13):
+                barcode_type = 'EAN13'
+                if len(value) in (11, 12):
+                    value = '0%s' % value
+            try:
+                width, height, humanreadable = int(width), int(height), bool(int(humanreadable))
+                rst = createBarcodeImageInMemory(barcode_type, value=value, format='svg', width=width, height=height,
+                    humanReadable=humanreadable)
+                e = base64.b64encode(bytes(rst, 'ascii'))
+                return 'data:image/svg+xml;base64,' + str(e)[2:-1]
+            except (ValueError, AttributeError):
+                if barcode_type == 'Code128':
+                    raise ValueError("Cannot convert into svg barcode.")
+                else:
+                    return self.svg_barcode('Code128', value, width=width, height=height, humanreadable=humanreadable)
+        else:
+            raise UserError(f'An earlier version of reportlab is installed: {reportlab.__version__} \nVersion 3.5.49 is needed.',
+            'See the readme (installation) of the module for more information')
+    
+
+class IrQweb(models.AbstractModel):
+    _inherit = 'ir.qweb'
+
+    # with this method we automate the rendering proccess to change all the regular barcodes to the customs svg
+    def _compile_node(self, el, options):
+        if el.tag == "img" and any(((att=='t-att-src') and el.attrib[att].startswith("'/report/barcode/") and (' % ' in el.attrib[att])) for att in el.attrib):
+            barcode_before = el.attrib.pop('t-att-src')
+            el.set('t-att-src', "request.env['ir.actions.report'].svg_barcode" + barcode_before.split(' % ')[1])
+        return super(IrQWeb, self)._compile_node(el, options)
