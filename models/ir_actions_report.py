2f574fa3aa16dc40c74170d29609436f5aab03bc
David Moreno <david.moreno@alliantum.com>
2021-12-14 17:48:15 +0100

    [IMP] add compatibility with other modules extending barcodes

--- models/ir_actions_report.py
+++ models/ir_actions_report.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
+from urllib.parse import urlparse, parse_qs
+from odoo import api, models
 from reportlab.graphics.barcode import createBarcodeImageInMemory
-from odoo.exceptions import UserError
 import base64
+
 
 
 class IrActionsReport(models.Model):
@@ -11,26 +10,40 @@ class IrActionsReport(models.Model):
 
     # All this code is based on the original method to produce barcodes
     @api.model
-    def svg_barcode(self, barcode_type, value, width=300, height=30, humanreadable=0):
+    def reportlab_svg_barcode(self, type='', value='', width=300, height=30, humanreadable=0, **kwargs):
         """ Create a SVG barcode image.
 
         Returns a base64 svg image ready to put it on a `src` attribute of an `<img>` tag
         """
-        if barcode_type == 'UPCA' and len(value) in (11, 12, 13):
-            barcode_type = 'EAN13'
+        if type == 'UPCA' and len(value) in (11, 12, 13):
+            type = 'EAN13'
             if len(value) in (11, 12):
                 value = '0%s' % value
         try:
             width, height, humanreadable = int(width), int(height), bool(int(humanreadable))
-            rst = createBarcodeImageInMemory(barcode_type, value=value, format='svg', width=width, height=height,
+            rst = createBarcodeImageInMemory(type, value=value, format='svg', width=width, height=height,
                 humanreadable=humanreadable)
             e = base64.b64encode(bytes(rst, 'ascii'))
-            return 'data:image/svg+xml;base64,' + str(e)[2:-1]
+            return 'data:image/svg+xml;base64,' + e.decode('utf-8')
         except (ValueError, AttributeError):
-            if barcode_type == 'Code128':
+            if type == 'Code128':
                 raise ValueError("Cannot convert into svg barcode.")
             else:
                 return self.svg_barcode('Code128', value, width=width, height=height, humanreadable=humanreadable)
+
+    @api.model
+    def get_svg_barcode(self, **kwargs):
+        # looking for modules extending the default type of barcodes
+        barcodes_extended_libraries = self.env['ir.config_parameter'].sudo().get_param('barcodes.extended_libraries')
+        if barcodes_extended_libraries:
+            for barcode_library in barcodes_extended_libraries.split(','):
+                if hasattr(self, f'{barcode_library}_svg_barcode'):
+                    barcode_value = getattr(self, f'{barcode_library}_svg_barcode')(**kwargs)
+                    if barcode_value:
+                        return barcode_value
+        # if our barcode is not yet generated, we always fallback to reportlab, which is the library used by default in Odoo
+        return self.reportlab_svg_barcode(**kwargs)
+
 
 
 class IrQWeb(models.AbstractModel):
@@ -38,11 +51,21 @@ class IrQWeb(models.AbstractModel):
 
     # with this method we automate the rendering proccess to change all the regular barcodes to the customs svg
     def _compile_node(self, el, options):
-        if el.tag == "img" and any(((att=='t-att-src') and el.attrib[att].startswith("'/report/barcode/") and (' % ' in el.attrib[att])) for att in el.attrib):
+        # this is just working for urls of the shape '/report/barcode?params' but not those like '/report/barcode/<type>/<path:value>'. Also the param values must be interpolated using the % symbol
+        if el.tag == "img" and any(att == 't-att-src' and el.attrib[att].startswith("'/report/barcode/?") and (' % ' in el.attrib[att]) for att in el.attrib):
             barcode_before = el.attrib.pop('t-att-src')
-            args = barcode_before.split(' % ')[1]
-            if "humanreadable=1" not in barcode_before:
-                el.set('t-att-src', "request.env['ir.actions.report'].svg_barcode" + args)
+            [url, args] = barcode_before.split(' % ')
+
+            keys = parse_qs(urlparse(url).query)
+            vals = [val.strip() for val in args.strip('(').strip(')').split(',')]
+            kwargs_string = ''
+            for key, val in zip(keys, vals):
+                kwargs_string += f'{key}={val}, '
+            # remove trailing ', ' after concatenating
+            kwargs_string = kwargs_string.strip(', ')
+
+            if 'type' in keys and 'value' in keys and "humanreadable=1" not in barcode_before:
+                el.set('t-att-src', f"request.env['ir.actions.report'].get_svg_barcode({kwargs_string})")
             else:
                 el.set('t-att-src', barcode_before)
         return super(IrQWeb, self)._compile_node(el, options)
